afe8fb42de64cc3699c511dbbee50f872a95013b
Dev Sebastian <devsebastian@gmail.com>
2020-03-24 23:15:50 +0530

    added credits

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Container from './comps/container/container'
 function App() {
   return (
     <div className="App">
-      <div class="credits">App Developed by <font color="#3cb4e0">Dev Sebastian</font></div>
+      <div class="credits"><a href="https://github.com/devsebastian">App Developed by <font color="#3cb4e0">Dev Sebastian</font></a></div>
       <Container />
     </div>
   );
