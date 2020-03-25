e1835517a3c5a7fba10eef65d06e8a71c9fc559f
Dev Sebastian <devsebastian@gmail.com>
2020-03-25 14:46:33 +0530

    support for multiple languages added

--- src/comps/container/container.js
+++ src/comps/container/container.js
@@ -7,7 +7,7 @@ import helpCircle from '../../assets/help-circle.svg'
 import headphone from '../../assets/headphone.svg'
 import call from '../../assets/call.svg'
 import rightCaret from '../../assets/right-caret.svg'
-import { getQuestions, yesButton, noButton, nextButton, previousButton } from '../../assets/data'
+import { medicalAdvice, intro, transportationTitle, assistanceTitle, startButtonTitle, helplineNumbers, warningTextOne, warningTextTwo, questions, yesAnswerTitle, noAnswerTitle, nextButtonTitle, previousButtonTitle, CDCmessage, locationConditionalLabel, bodyTemperatureConditionalLabel, countryWarning, riskWarning, faqTitle } from '../../assets/data'
 import Progress from '../progress/progress'
 import QuestionContainer from '../question-container/question-container'
 import AnswerContainer from '../answer-container/answer-container'
@@ -24,35 +24,33 @@ function ResultButton({ title, leftIcon, rightIcon, link, subtitle }) {
     </a>
 }
 
-function Result({ statements }) {
-    var CDCmessage = <font style={{ color: "#4caf50" }}><img width="40%" src={check} alt="checked" style={{ display: "block", margin: "32px auto" }} /><b>As per CDC and WHO Guidelines, you are not a likely suspect of COVID-19.</b></font>
+function Result({ answers, language }) {
+    var CDCmessageBlock = <font style={{ color: "#4caf50" }}><img width="40%" src={check} alt="checked" style={{ display: "block", margin: "32px auto" }} /><b>{CDCmessage[language]}</b></font>
 
-    var warning = (<div>
-        Incase your symptoms change, please take the self-assessment again or contact any healthcare facility near you.  <b><a target="_blank" rel="noopener noreferrer" href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf">helpline numbers.</a></b>
-        <br /><br />
-        In case you suspect a positive case, please <a href="tel:1075"><b>call 1075</b></a> or report to a nearby healthcare facility</div>)
-    var result = <div>{CDCmessage}<br /><br />{warning}</div>
+    var warning = (<div> {warningTextOne[language]}  <b><a target="_blank" rel="noopener noreferrer" href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf">{helplineNumbers[language]}</a></b>
+        <br /><br />{warningTextTwo[language]}</div>)
+    var result = <div>{CDCmessageBlock}<br /><br />{warning}</div>
 
     var countries = ["china", "america", "usa", "united states of america", "italy", "iran", "south korea", "mainland china"]
 
-    if (statements[0].answer === "yes" && countries.includes(statements[0].conditionalAnswer.toLowerCase())) {
+    if (answers[0].answer === "yes" && countries.includes(answers[0].conditionalAnswer.toLowerCase())) {
         result = <div>
-            {CDCmessage}<br /><br />DO not attend work and self-quarantine for 14 days after leaving the country{warning}</div>
+            {CDCmessageBlock}<br /><br />{countryWarning}{warning}</div>
     }
 
-    if (statements[1].answer === "yes") {
-        result = <div><div>{CDCmessage}</div><br /><br />Self Quarantine for 14 days from the last day of contact with the confirmed case<div><br />{warning}</div></div>
+    if (answers[1].answer === "yes") {
+        result = <div><div>{CDCmessageBlock}</div><br /><br />Self Quarantine for 14 days from the last day of contact with the confirmed case<div><br />{warning}</div></div>
     }
 
 
-    if ((statements[2].answer === "yes" && statements[2].conditionalAnswer > 100 && statements[3].answer === "yes") ||
-        (statements[2].answer === "yes" && statements[2].conditionalAnswer > 100 && statements[4].answer === "yes") ||
-        (statements[1].answer === "yes" && statements[3].answer === "yes" && statements[4].answer === "yes")) {
+    if ((answers[2].answer === "yes" && answers[2].conditionalAnswer > 100 && answers[3].answer === "yes") ||
+        (answers[2].answer === "yes" && answers[2].conditionalAnswer > 100 && answers[4].answer === "yes") ||
+        (answers[1].answer === "yes" && answers[3].answer === "yes" && answers[4].answer === "yes")) {
         result = <div>
-            <img width="30%" src={corona} alt="corona" style={{ display: "block", margin: "32px auto" }} /> Based on the assessment, you are at risk for COVID-19
-        <ResultButton link="https://www.mohfw.gov.in/pdf/FAQ.pdf" title="FAQ" leftIcon={helpCircle} rightIcon={rightCaret} />
-            <ResultButton link="tel:108" title="Transporation to isolation ward" subtitle="Disaster management service: 108" leftIcon={ambulance} rightIcon={call} />
-            <ResultButton link="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf" title="For any assistance" subtitle="contact the govt. helpline: 1075" leftIcon={headphone} rightIcon={rightCaret} />
+            <img width="30%" src={corona} alt="corona" style={{ display: "block", margin: "32px auto" }} /> {riskWarning[language]}
+            <ResultButton link="https://www.mohfw.gov.in/pdf/FAQ.pdf" title={faqTitle[language]} leftIcon={helpCircle} rightIcon={rightCaret} />
+            <ResultButton link="tel:108" title={transportationTitle[language]} subtitle="Disaster management service: 108" leftIcon={ambulance} rightIcon={call} />
+            <ResultButton link="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf" title={assistanceTitle[language]} subtitle="contact the govt. helpline: 1075" leftIcon={headphone} rightIcon={rightCaret} />
             <br />
         </div>
     }
@@ -61,30 +59,67 @@ function Result({ statements }) {
     return (<div className="result">
         {result}
         <br />
-        <div className="result-extra">*This is not a medical advice</div>
+        <div className="result-extra">{medicalAdvice[language]}</div>
     </div>)
 }
 
 export default class Container extends React.Component {
-    locationInput = <div className="pane-conditional"><label htmlFor="location-input" >Which country / Indian state did you travel?</label><br /><input id="location-input" onChange={(e) => this.handleChange(e.target.value)} />  </div>
-    temperatureInput = <div className="pane-conditional"><label htmlFor="location-input" >Body temperature (°F)</label><br /><input id="temperature-input" onChange={(e) => this.handleChange(e.target.value)} />  </div>
-
-    questions = getQuestions(this.locationInput, this.temperatureInput);
+    locationInput = <div className="pane-conditional"><label htmlFor="location-input" >{locationConditionalLabel[this.props.language]}</label><br /><input id="location-input" onChange={(e) => this.handleChange(e.target.value)} />  </div>
+    temperatureInput = <div className="pane-conditional"><label htmlFor="location-input" >{bodyTemperatureConditionalLabel[this.props.language]}</label><br /><input id="temperature-input" onChange={(e) => this.handleNumbers(e.target.value)} />  </div>
 
     handleChange(val) {
         this.setState(oldState => {
-            var statements = oldState.statements
-            statements[this.state.questionNumber].conditionalAnswer = val
-            return { statements: statements }
+            var answers = oldState.answers
+            answers[this.state.questionNumber].conditionalAnswer = val
+            return { answers: answers }
         })
     }
 
-    constructor() {
-        super()
+
+    handleNumbers(val) {
+        this.setState(oldState => {
+            var answers = oldState.answers
+            if (isNaN(val)) {
+                answers[this.state.questionNumber].conditionalAnswer = undefined
+            } else {
+                answers[this.state.questionNumber].conditionalAnswer = val
+            }
+            console.log(answers)
+
+            return { answers: answers }
+        })
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.locationInput = <div className="pane-conditional"><label htmlFor="location-input" >{locationConditionalLabel[nextProps.language]}</label><br /><input id="location-input" onChange={(e) => this.handleChange(e.target.value)} />  </div>
+        this.temperatureInput = <div className="pane-conditional"><label htmlFor="location-input" >{bodyTemperatureConditionalLabel[nextProps.language]}</label><br /><input id="temperature-input" onChange={(e) => this.handleNumbers(e.target.value)} />  </div>
+        this.setState(oldState => {
+            var ans = oldState.answers
+            ans[0].conditional = this.locationInput
+            ans[2].conditional = this.temperatureInput
+            return { answers: ans }
+        })
+    }
+
+    constructor(props) {
+        super(props)
         this.state = {
             questionNumber: -1,
-            language: "english",
-            statements: this.questions["english"],
+            answers: [
+                {
+                    answer: undefined,
+                    conditional: this.locationInput
+                },
+                { answer: undefined },
+                {
+                    answer: undefined,
+                    conditional: this.temperatureInput
+                },
+                { answer: undefined },
+                { answer: undefined },
+                { answer: undefined },
+            ],
         }
 
 
@@ -92,11 +127,10 @@ export default class Container extends React.Component {
         this.goToPreviousQuestion = this.goToPreviousQuestion.bind(this)
         this.setAnswerToYes = this.setAnswerToYes.bind(this)
         this.setAnswerToNo = this.setAnswerToNo.bind(this)
-
     }
 
     goToNextQuestion() {
-        if (this.state.questionNumber < this.state.statements.length) {
+        if (this.state.questionNumber < this.state.answers.length) {
             this.setState(oldState => {
                 return { questionNumber: oldState.questionNumber + 1 }
             })
@@ -113,80 +147,69 @@ export default class Container extends React.Component {
 
     setAnswerToYes(pos) {
         this.setState(oldState => {
-            var statements = oldState.statements
-            statements[pos].answer = "yes"
-            return { statements: statements }
+            var answers = oldState.answers
+            answers[pos].answer = "yes"
+            return { answers: answers }
         })
     }
 
     setAnswerToNo(pos) {
         this.setState(oldState => {
-            var statements = oldState.statements
-            statements[pos].answer = "no"
-            return { statements: statements }
+            var answers = oldState.answers
+            answers[pos].answer = "no"
+            return { answers: answers }
         })
     }
 
 
     render() {
-        const total = this.state.statements.length
-        const { questionNumber, language } = this.state
+        const total = this.state.answers.length
+        const { questionNumber } = this.state
+        const language = this.props.language
+        var body = <div></div>
         if (questionNumber < 0) {
-            return <PaneContainer
-                questionNumber={questionNumber}
-                total={total}
-                body={<div class="statement-container">This web app is a COVID-19 Self Assessment Tool.
-                <br /><br /><small><b><font color="gray">*This is not a medical advice.</font></b></small>
-                    <br />
-                    <br />
-                    <div class="control-btn-container">
-                        <div class="next-btn next-btn-enabled" onClick={this.goToNextQuestion}>
-                            Next
-                        </div>
+            body = <div className="statement-container">{intro[language]}
+                <br /><br /><small><b><font color="gray">{medicalAdvice[language]}</font></b></small>
+                <br />
+                <br />
+                <div className="control-btn-container">
+                    <div className="next-btn next-btn-enabled" onClick={this.goToNextQuestion}>
+                        {startButtonTitle[language]}
                     </div>
-                </div>}
-            />
-        }
-        else if (questionNumber < total) {
-            const statement = this.state.statements[questionNumber]
-
-            const { answer, question, conditional } = statement
-
-            const yesButtonTitle = yesButton[language]
-            const noButtonTitle = noButton[language]
-            const previousButtonTitle = previousButton[language]
-            const nextButtonTitle = nextButton[language]
-            return (
-                <PaneContainer
-                    questionNumber={questionNumber}
-                    total={total}
-                    body={<Pane
-                        questionNumber={questionNumber}
-                        total={total}
-                        question={question}
-                        answer={answer}
-                        conditional={conditional}
-                        yesButtonTitle={yesButtonTitle}
-                        noButtonTitle={noButtonTitle}
-                        statement={statement}
-                        previousButtonTitle={previousButtonTitle}
-                        nextButtonTitle={nextButtonTitle}
-                        goToPreviousQuestion={this.goToPreviousQuestion}
-                        goToNextQuestion={this.goToNextQuestion}
-                        setAnswerToYes={this.setAnswerToYes}
-                        setAnswerToNo={this.setAnswerToNo}
-                    />}
-                />
-
-
-            )
-        } else {
-            return <PaneContainer
+                </div>
+            </div>
+        } else if (questionNumber < total) {
+            const statement = this.state.answers[questionNumber]
+            const { answer, conditional } = statement
+            const question = questions[this.props.language][questionNumber]
+            const yesAnswerTitleTitle = yesAnswerTitle[language]
+            const noAnswerTitleTitle = noAnswerTitle[language]
+            const previousButtonTitleTitle = previousButtonTitle[language]
+            const nextButtonTitleTitle = nextButtonTitle[language]
+            body = <Pane
                 questionNumber={questionNumber}
                 total={total}
-                body={<Result statements={this.state.statements} />}
+                question={question}
+                answer={answer}
+                conditional={conditional}
+                yesAnswerTitleTitle={yesAnswerTitleTitle}
+                noAnswerTitleTitle={noAnswerTitleTitle}
+                statement={statement}
+                previousButtonTitleTitle={previousButtonTitleTitle}
+                nextButtonTitleTitle={nextButtonTitleTitle}
+                goToPreviousQuestion={this.goToPreviousQuestion}
+                goToNextQuestion={this.goToNextQuestion}
+                setAnswerToYes={this.setAnswerToYes}
+                setAnswerToNo={this.setAnswerToNo}
             />
+        } else {
+            body = <Result answers={this.state.answers} language={this.props.language} />
         }
+        return <PaneContainer
+            questionNumber={questionNumber}
+            total={total}
+            body={body}
+        />
     }
 }
 
@@ -198,7 +221,7 @@ function PaneContainer({ questionNumber, total, body }) {
     </div>
 }
 
-function Pane({ questionNumber, total, answer, question, conditional, yesButtonTitle, noButtonTitle, setAnswerToYes, setAnswerToNo, statement, goToPreviousQuestion, goToNextQuestion, previousButtonTitle, nextButtonTitle }) {
+function Pane({ questionNumber, total, answer, question, conditional, yesAnswerTitleTitle, noAnswerTitleTitle, setAnswerToYes, setAnswerToNo, statement, goToPreviousQuestion, goToNextQuestion, previousButtonTitleTitle, nextButtonTitleTitle }) {
     return <div className="pane">
         <div className="statement-container">
             <QuestionContainer
@@ -209,10 +232,10 @@ function Pane({ questionNumber, total, answer, question, conditional, yesButtonT
                 conditional={conditional}
                 setAnswerToYes={() => setAnswerToYes(questionNumber)}
                 setAnswerToNo={() => setAnswerToNo(questionNumber)}
-                yesButtonTitle={yesButtonTitle}
-                noButtonTitle={noButtonTitle} />
+                yesAnswerTitle={yesAnswerTitleTitle}
+                noAnswerTitle={noAnswerTitleTitle} />
         </div>
-        <ButtonsContainer isNextEnabled={isNextEnabled(statement)} goToPreviousQuestion={goToPreviousQuestion} goToNextQuestion={goToNextQuestion} isLastQuestion={isLastQuestion(questionNumber, total)} previous={previousButtonTitle} next={nextButtonTitle} />
+        <ButtonsContainer isNextEnabled={isNextEnabled(statement)} goToPreviousQuestion={goToPreviousQuestion} goToNextQuestion={goToNextQuestion} isLastQuestion={isLastQuestion(questionNumber, total)} previous={previousButtonTitleTitle} next={nextButtonTitleTitle} />
     </div>
 }
 
