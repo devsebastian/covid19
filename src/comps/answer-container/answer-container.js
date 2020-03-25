2dc27bee720afdb66248ea5733ec6fc93517333f
Dev Sebastian <devsebastian@gmail.com>
2020-03-25 14:46:20 +0530

    change in nomenclature

--- src/comps/answer-container/answer-container.js
+++ src/comps/answer-container/answer-container.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import './answer-container.css'
 
-export default function AnswerContainer({ answer, conditional, setAnswerToYes, setAnswerToNo, yesButtonTitle, noButtonTitle }) {
+export default function AnswerContainer({ answer, conditional, setAnswerToYes, setAnswerToNo, yesAnswerTitle, noAnswerTitle }) {
     return <div className="answer-container">
         <div className="question-number">Answer Options</div>
-        <button className={answer === "yes" ? "option-btn option-btn-selected" : "option-btn"} onClick={setAnswerToYes} >{yesButtonTitle}</button>
+        <button className={answer === "yes" ? "option-btn option-btn-selected" : "option-btn"} onClick={setAnswerToYes} >{yesAnswerTitle}</button>
         {answer === "yes" && <div className="conditional">{conditional}</div>}
-        <button className={answer === "no" ? "option-btn option-btn-selected" : "option-btn"} onClick={setAnswerToNo} >{noButtonTitle}</button>
+        <button className={answer === "no" ? "option-btn option-btn-selected" : "option-btn"} onClick={setAnswerToNo} >{noAnswerTitle}</button>
     </div>
 }
